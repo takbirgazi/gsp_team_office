0e0b2d6402fcbedb52377fdd1fcff990408f685b
takbirgazi <takbirgazibd@gmail.com>
2024-01-17 16:34:55 +0600

    Add javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,15 @@ consult.addEventListener("click", function(){
     if(cosult_desc.classList.contains("hide")){
         cosult_desc.classList.remove("hide");
         team_desc.classList.add("hide");
+        consult.classList.add("active_2");
+        team.classList.remove("active")
     }
 })
 team.addEventListener("click", function(){
     if(team_desc.classList.contains("hide")){
         team_desc.classList.remove("hide");
         cosult_desc.classList.add("hide");
+        team.classList.add("active");
+        consult.classList.remove("active_2");
     }
 })
